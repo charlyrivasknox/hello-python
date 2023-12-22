f6467559037429951e8a337dc6cf6c31c549d6f3
Carlos <charlyrivasknox@gmail.com>
2023-12-22 13:23:49 -0600

    Create hello.py
    prueba inicial de git

---
+++ Moure/Hello-python/hello.py
@@ -0,0 +1,15 @@
+# Esto es un comentario:0
+# Nuestro Hola Mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Esto es un comentario en 
+varias lineas
+"""
+
+'''
+Esto es un comentario en 
+varias lineas tambien
+'''
+
