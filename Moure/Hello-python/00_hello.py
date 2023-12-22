a03768fd6fb26f3748d6cad7559e311a96bd9a3a
Carlos <charlyrivasknox@gmail.com>
2023-12-22 14:17:09 -0600

    update all

--- Moure/Hello-python/00_hello.py
+++ Moure/Hello-python/00_hello.py
@@ -15,4 +15,12 @@ Esto es un comentario en
 varias lineas tambien
 ESTO CAMBIA DESDE GITHUB
 '''
+# Consultar el tipo de dato
+print(type("Soy un dato str")) #Tipo 'str'
+print(type(5)) #Tipo 'int'
+print(type(1.5)) #Tipo 'float'
+print(type(True)) #Tipo 'bool'
+print(type(3+1j)) #Tipo 'complex'
+
+
 
